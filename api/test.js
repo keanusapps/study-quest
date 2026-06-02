61016b8ae26c869d1dd60c798a81c95f0bac1f21
keanusapps <keanu.maccagnan13@gmail.com>
2026-06-02 15:52:07 +0200

    Delete api/test.js

--- api/test.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  res.status(200).json({
-    hasKey: !!process.env.GEMINI_API_KEY,
-    keyLength: process.env.GEMINI_API_KEY?.length || 0,
-    allKeys: Object.keys(process.env).filter(k => !k.toLowerCase().includes('secret') && !k.toLowerCase().includes('token')),
-    nodeVersion: process.version
-  });
-}
- 
